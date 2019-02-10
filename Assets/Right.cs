1fe1be3a0a6b493acfcd47407ca892121c2bc967
DianeLam <43661044+DianeLam@users.noreply.github.com>
2019-02-10 03:33:54 -0600

    Create Right.cs

---
+++ Assets/Right.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+
+public class Right : MonoBehaviour
+{
+    public Vector3 pos;
+    public Vector3 oldpos;
+    // Use this for initialization
+    private bool rights = true;
+
+    void Start()
+    {
+        oldpos = transform.position;
+        pos = transform.position;
+    }
+
+
+    void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (rights)
+            {
+
+                transform.position = new Vector3(pos.x +3, pos.y, pos.z);
+
+
+                rights = false;
+            }
+            else
+            {
+                transform.position = new Vector3(oldpos.x, pos.y, pos.z);
+
+
+                rights = true;
+            }
+        }
+    }
+
+}
