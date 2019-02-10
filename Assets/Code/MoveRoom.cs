f9c9bae5e987acd374c109ddfb8e457b5fe3aea7
DianeLam <43661044+DianeLam@users.noreply.github.com>
2019-02-09 23:53:04 -0600

    Create MoveRoom.cs

---
+++ Assets/Code/MoveRoom.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRoom : MonoBehaviour {
+    public float speed = 5.0f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+        }
+
+    }
+}
