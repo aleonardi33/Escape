ca0b373561ae0fb3b0bbcaef92a8c520653893ae
DianeLam <43661044+DianeLam@users.noreply.github.com>
2019-02-09 23:53:20 -0600

    Create Timer.cs

---
+++ Assets/Code/Timer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Timer : MonoBehaviour {
+
+    public GUIStyle GUIStyle;
+
+    /// <summary>
+    /// Screen location for the time.
+    /// </summary>
+    public Rect ScreenLocation;
+    bool times = true;
+    private int minutes;
+
+
+    public static float TimeRemaining;
+
+    void Update()
+    {
+        if (times)
+        {
+            UpdateTime();
+        }
+        if (minutes == 1)
+        {
+            Stoptime();
+        }
+    }
+    void UpdateTime()
+    {
+    
+      TimeRemaining += Time.deltaTime;
+
+    }
+
+    void Stoptime()
+    {
+
+        times = false;
+
+    }
+
+    internal void OnGUI()
+    {
+        minutes = Mathf.FloorToInt(TimeRemaining / 60F);
+        int seconds = Mathf.FloorToInt(TimeRemaining - minutes * 60);
+        string format = string.Format("{0:0}:{1:00}", minutes, seconds);
+        GUIStyle.fontSize = 30;
+        GUI.Label(ScreenLocation, "Remaining Time: " + format, GUIStyle);
+    }
+}
