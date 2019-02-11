165a1882da44ac9f63bffa2538f272510c1fe557
3y35py <lizasalvi2021@users.noreply.github.com>
2019-02-11 14:33:08 -0600

    Instructions + A/D key input
    Added the option to use the A/D keys to move across the room in addition to the left/right arrows. Added simple instructions on the bottom of the screen letting the player know that they can move in such a way and click on objects to interact with them. These instructions disappear once the player moves.

--- Assets/Code/MoveRoom.cs
+++ Assets/Code/MoveRoom.cs
@@ -4,26 +4,42 @@ using UnityEngine;
 
 public class MoveRoom : MonoBehaviour {
     
+	public GUIStyle GUIStyle;
+	public Rect ScreenLocation;
+	
     public float speed = 5.0f;
 	public float leftlimit = -3;
 	public float rightlimit = 10;
+	private bool hasmoved;
     
     // Use this for initialization
-    void Start () {
-		
+    void Start ()
+    {
+	    hasmoved = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.GetKey(KeyCode.RightArrow) && transform.position.x < rightlimit && GameObject.Find("numpadlarge") == null)
+		if ((Input.GetKey(KeyCode.RightArrow)||Input.GetKey(KeyCode.D)) && transform.position.x < rightlimit && GameObject.Find("numpadlarge") == null)
 		{
 			transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+			hasmoved = true;
 		}
 
-		if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x > leftlimit && GameObject.Find("numpadlarge") == null)
+		if ((Input.GetKey(KeyCode.LeftArrow)||Input.GetKey(KeyCode.A)) && transform.position.x > leftlimit && GameObject.Find("numpadlarge") == null)
 		{
 			transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+			hasmoved = true;
+		}
+	}
+
+	void OnGUI()
+	{
+		if (!hasmoved)
+		{
+			GUIStyle.fontSize = 12;
+			GUI.Label(ScreenLocation, "Use the arrow keys or A/D to move left or right across the room. Click on objects to interact with them.", GUIStyle);
 		}
 	}
 }
