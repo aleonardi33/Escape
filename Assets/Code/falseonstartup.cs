4bb133a3b2fd2a5fb154d834212ea071709b2523
Tyler Rodgers <31381200+tylrodg@users.noreply.github.com>
2019-02-12 17:24:48 -0600

    Exit Room 1, enter Room 2

--- Assets/Code/falseonstartup.cs
+++ Assets/Code/falseonstartup.cs
@@ -1,16 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+public class falseonstartup : MonoBehaviour
+{
 
-public class falseonstartup : MonoBehaviour {
+	int nextSceneBuildIndex = 0;
+    // Use this for initialization
+    void Start()
+    {
+		nextSceneBuildIndex = SceneManager.GetActiveScene().buildIndex;
+        gameObject.SetActive(false);
+    }
 
-	// Use this for initialization
-	void Start () {
-		gameObject.SetActive(false);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    void OnMouseUp()
+    {
+        if (gameObject.activeSelf)
+        {
+            SceneManager.LoadScene(nextSceneBuildIndex + 1);
+        }
+    }
 }
