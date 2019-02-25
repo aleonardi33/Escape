d1ce530655d71bcc2ade973e51b2486e463c9d27
Tyler Rodgers <31381200+tylrodg@users.noreply.github.com>
2019-02-25 09:23:17 -0600

    Add hallway

---
+++ Assets/Code/HallwayNumpad.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HallwayNumpad : MonoBehaviour
+{
+	private Transform dooropen;
+	// Use this for initialization
+	void Start()
+	{
+		foreach (Transform sibling in transform.parent)
+		{
+			if (sibling.name.Equals("elevator-open"))
+			{
+				dooropen = sibling;
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+	void OnMouseUp()
+	{		
+		if (!dooropen.gameObject.activeSelf)
+		{
+			transform.GetChild(0).gameObject.GetComponent<largenumpad>().ToggleColliders(false);
+			GetComponent<AudioSource>().Play();
+			foreach (Transform child in transform)
+			{
+				child.gameObject.SetActive(true);
+			}
+		}
+	}
+}
