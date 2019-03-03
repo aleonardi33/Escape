6acdf41aa2172c0b7741f326250c43ba0fe0d6ca
Tyler Rodgers <31381200+tylrodg@users.noreply.github.com>
2019-03-03 17:27:27 -0600

    moving background

---
+++ Assets/Code/RepeatingBackground.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingBackground : MonoBehaviour
+{
+    private BoxCollider2D skyCollide;
+    private float skyHorizLen;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        skyCollide = GetComponent<BoxCollider2D>();
+        skyHorizLen = skyCollide.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Check if the difference along the x axis between the main Camera and the position of the object this is attached to is greater than groundHorizontalLength.
+        if (transform.position.x < -skyHorizLen)
+        {
+            //If true, this means this object is no longer visible and we can safely move it forward to be re-used.
+            RepositionBackground();
+        }
+    }
+    private void RepositionBackground()
+    {
+        Vector2 skyOffset = new Vector2(skyHorizLen * 2f, 0);
+        transform.position = (Vector2)transform.position + skyOffset;
+    }
+}
---
+++ Assets/Code/ScrollingObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingObject : MonoBehaviour 
+{
+    private Rigidbody2D rb2d;
+    private float scrollSpeed =-1.5f;
+    // Use this for initialization
+    void Start () 
+    {
+        //Get and store a reference to the Rigidbody2D attached to this GameObject.
+        rb2d = GetComponent<Rigidbody2D>();
+
+        //Start the object moving.
+        rb2d.velocity = new Vector2 (scrollSpeed, 0);
+    }
+
+    void Update()
+    {
+
+    }
+}
