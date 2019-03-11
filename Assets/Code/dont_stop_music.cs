49e5d90c6db4e7d53ddb29011f2fc0fe2f1c05a7
aleonardi33 <anthonyleonardi2020@u.northwestern.edu>
2019-03-10 19:28:22 -0500

    music

---
+++ Assets/Code/dont_stop_music.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+
+public class dont_stop_music : MonoBehaviour{
+    public AudioClip audclp;
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "Final ROom"||SceneManager.GetActiveScene().name=="Right")
+        {
+            audclp = Resources.Load<AudioClip>("Escape Song Scary");
+        }
+        if ((SceneManager.GetActiveScene().name == "Main Menu"||SceneManager.GetActiveScene().name=="Left"))
+        {
+            audclp = Resources.Load<AudioClip>("Escape Song Scary");
+        }
+    }
+}
